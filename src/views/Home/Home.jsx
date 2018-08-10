2f022e10347bca3c019722f78f9429d1b4a69d59
francesco.megna97 <francesco.megna97@gmail.com>
2018-08-10 15:26:53 +0200

    minor

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,22 +1,18 @@
 import React from 'react';
 
+const proxyUrl = "https://cors-anywhere.herokuapp.com/"
+const client_id = "8f0ce6cc11104955a7504b845b0aa02a"
+
 const Home = ({
 
 }) => {
 
-  const redirectToPrompt = () => {
-    const client_id = "8f0ce6cc11104955a7504b845b0aa02a"
-    fetch(`https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=token&redirect_uri=http://localhost:3000/`).then(res => {
-        console.log(res)
-      })
-  }
-
   return (
     <div style={{ margin: 40 }}>
       <h2>Homepage</h2>
-      <button onClick={redirectToPrompt}>
+      <a href={`${proxyUrl}https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=token&redirect_uri=http://localhost:3000/`}>
         login with spotify
-      </button>
+      </a>
     </div>
   )
 }
